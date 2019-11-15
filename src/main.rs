625d1f0d0a581e5dc2a960df420c21ead3cc7b58
Zach Clayburn <zachclayburn@gmail.com>
2019-11-14 17:51:03 -0700

    Got a simple login form working

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,31 @@ extern crate rocket;
 
 use rocket::response::Redirect;
 use rocket_contrib::serve::StaticFiles;
+use rocket::request::{Form, FormError, FormDataError, FormParseError};
 
 #[get("/", rank = 2)]
 fn index() -> Redirect {
     Redirect::to("/index.html")
 }
 
+#[derive(Debug, FromForm)]
+struct Login {
+    username: String,
+    password: String,
+}
+
+#[post("/login", data = "<login>")]
+fn login(login: Result<Form<Login>, FormError>) -> String {
+    match login {
+        Ok(login) => {format!("{:?}", &*login)},
+        Err(err) => {format!("{:?}", err)},
+    }
+}
+
 fn main() {
     rocket::ignite()
         .mount("/", StaticFiles::from("static/"))
         .mount("/", routes![index])
+        .mount("/users", routes![login])
         .launch();
 }
